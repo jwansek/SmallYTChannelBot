be4875fa0dc1d534df3504861dbd0991ac5e27bd
jwansek <37976823+jwansek@users.noreply.github.com>
2018-12-16 16:16:48 +0000

    Deleted a file

--- login.py
+++
@@ -1,7 +0,0 @@
-import praw
-
-REDDIT = praw.Reddit(client_id = "PyyyRXa3veWsWA",
-                    client_secret = "hAMbhuzdHIew5hmn1CFwWb5FurM",
-                    user_agent = "SmallYTChannelBot",
-                    username = "SmallYTChannelBot",
-                    password = "6NEWGNPBjJjbOjk3lbtm")
